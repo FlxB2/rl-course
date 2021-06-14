084f4af0751b46ad588b182a5277c404f0f41e9b
pascalhuszar <pascal.huszar@live.de>
2021-06-14 21:00:46 +0200

    initial code commit

--- ex07-fa/ex07-fa.py
+++ ex07-fa/ex07-fa.py
@@ -1,27 +1,120 @@
+#!/usr/bin/env python3
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def random_episode(env):
-    """ This is an example performing random actions on the environment"""
-    while True:
-        env.render()
-        action = env.action_space.sample()
-        print("do action: ", action)
-        observation, reward, done, info = env.step(action)
-        print("observation: ", observation)
-        print("reward: ", reward)
-        print("")
-        if done:
-            break
+EPISODE_LOG = 50
+
+BUCKET_AMOUNT = [20, 20]
+env = gym.make('MountainCar-v0')
+BUCKET_SIZE = (env.observation_space.high - env.observation_space.low) / BUCKET_AMOUNT
+
+
+def get_discrete_state(state):
+    discrete_state = (state - env.observation_space.low) / BUCKET_SIZE
+    return tuple(discrete_state.astype(np.int32))
+
+
+# tune learning rate
+def qlearning(env, q_table, alpha=0.001, gamma=0.9, epsilon=0.1,
+              initial_learning_rate=1.0, min_learning_rate=0.005, num_ep=int(5000)):
+
+    episode_rewards = []
+    episode_lengths = []
+    episode_hits = []
+    aggr_ep_rewards = {'ep': [], 'avg': [], 'max': [], 'min': []}
+
+    for episode in range(num_ep):
+        episode_reward = 0
+        episode_length = 0
+        reached_goal = 0
+
+        state = env.reset()
+        discrete_state = get_discrete_state(state)
+        done = False
+
+
+        while not done:
+            if np.random.uniform(0, 1) > epsilon:
+                action = np.argmax(q_table[discrete_state])
+            else:
+                action = env.action_space.sample()
+
+            new_state, reward, done, _ = env.step(action)
+            new_discrete_state = get_discrete_state(new_state)
+
+            '''render if wanted
+            if (episode + 1) % 50 == 0:
+                env.render()
+            '''
+            q_table[discrete_state + (action,)] += alpha * (reward + gamma *
+                                                                    np.max(q_table[new_discrete_state]) -
+                                                                    q_table[discrete_state + (action,)])
+
+            if new_state[0] >= env.goal_position:
+                reached_goal += 1
+
+            discrete_state = new_discrete_state
+
+            episode_reward += reward
+            episode_length += 1
+
+        episode_rewards.append(episode_reward)
+        episode_lengths.append(episode_length)
+        episode_hits.append(reached_goal)
+
+        if not episode % EPISODE_LOG:
+            if episode == 0:
+                average_reward = episode_rewards[0]
+            else:
+                average_reward = sum(episode_rewards[-EPISODE_LOG:]) / EPISODE_LOG
+            aggr_ep_rewards['ep'].append(episode)
+            aggr_ep_rewards['avg'].append(average_reward)
+            aggr_ep_rewards['max'].append(max(episode_rewards[-EPISODE_LOG:]))
+            aggr_ep_rewards['min'].append(min(episode_rewards[-EPISODE_LOG:]))
+
+    env.close()
+
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'],
+             label="aggregated average rewards of " + str(EPISODE_LOG) + " episodes")
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'],
+             label="aggregated max rewards of " + str(EPISODE_LOG) + " episodes")
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'],
+             label="aggregated min rewards of " + str(EPISODE_LOG) + " episodes")
+    plt.legend(loc=2)
+    plt.grid(True)
+    plt.savefig('reward.png')
+    # plt.show()
+    plt.clf()
+
+    return episode_hits, episode_lengths
 
 
 def main():
-    env = gym.make('MountainCar-v0')
-    env.reset()
-    random_episode(env)
+    reached_goals = []
+    episode_lengths = []
+    for i in range(10):
+        q_table = np.random.uniform(low=-2, high=0, size=(BUCKET_AMOUNT + [env.action_space.n]))
+        reached_goal, episode_length = qlearning(env, q_table)
+        reached_goals.append(reached_goal)
+        episode_lengths.append(episode_length)
     env.close()
+
+    episodes = [i for i in range(np.mean(reached_goals, axis=0).shape[0])]
+
+    plt.plot(episodes, np.mean(reached_goals, axis=0), label="reaching goal")
+    plt.legend(loc=2)
+    plt.grid(True)
+    plt.savefig('goal.png')
+    # plt.show()
+
+    plt.clf()
+    plt.plot(episodes, np.mean(episode_lengths, axis=0), label="episode length")
+    plt.legend(loc=2)
+    plt.grid(True)
+    plt.savefig('length.png')
+    # plt.show()
 
 
 if __name__ == "__main__":
