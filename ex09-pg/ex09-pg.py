9c0ded99b6bb04eee15d0248ba70e134958c1090
Felix Burk <felix.burk@googlemail.com>
2021-06-28 17:37:19 +0200

    adds ex 1 a,b,c

--- ex09-pg/ex09-pg.py
+++ ex09-pg/ex09-pg.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 
 def policy(state, theta):
     """ TODO: return probabilities for actions under softmax action selection """
-    return [0.5, 0.5]  # both actions with 0.5 probability => random
+    res = 1 / (1 + np.exp(-np.dot(state, theta)))
+    return [float(res[0]), float(1 - res[0])]
 
 
 def generate_episode(env, theta, display=False):
-    """ enerates one episode and returns the list of states, the list of rewards and the list of actions of that episode """
+    """ generates one episode and returns the list of states, the list of rewards and the list of actions of that
+    episode """
     state = env.reset()
     states = [state]
     actions = []
@@ -32,6 +34,11 @@ def generate_episode(env, theta, display=False):
 
 def REINFORCE(env):
     theta = np.random.rand(4, 2)  # policy parameters
+    T = 100
+    gamma = 0.5
+    alpha = 0.1
+    length = []
+    averages = []
 
     for e in range(10000):
         if e % 300 == 0:
@@ -41,16 +48,34 @@ def REINFORCE(env):
 
         print("episode: " + str(e) + " length: " + str(len(states)))
         # TODO: keep track of previous 100 episode lengths and compute mean
+        length.append(len(states))
+
+        if e > 100:
+            a = np.mean(length[-100:])
+            averages.append(a)
+        else:
+            a = 0
+
+        if a > 498:
+            break
 
         # TODO: implement the reinforce algorithm to improve the policy weights
-
-
-
+        for t in range(0, T - 1):
+            G = 0
+            for k in range(t + 1, len(states)):
+                G += gamma ** (k - t - 1) * rewards[k]
+            theta += alpha * gamma ** t * G * np.dot(np.array(states).T, (1 / (1 + np.exp(-np.dot(states, theta)))) - 1)
+    return averages
 
 
 def main():
     env = gym.make('CartPole-v1')
-    REINFORCE(env)
+    averages = REINFORCE(env)
+    plt.plot(range(0, len(averages)), averages)
+    plt.xlabel('Epochs')
+    plt.ylabel('Average length')
+    plt.show()
+    env.close()
     env.close()
 
 
